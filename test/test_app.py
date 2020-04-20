6bb582c8e957710e77c7498b2ad279bd0da65cba
Sendhello <sendhello@yahoo.com>
2020-04-20 22:19:40 +0300

    Редактирование тестов

--- test/test_app.py
+++ test/test_app.py
@@ -1,15 +1,11 @@
+import re
 from datetime import datetime
 
 import pytest
 from flask import url_for
 
 
-@pytest.mark.options(debug=False)
-def test_app(app):
-    assert not app.debug, 'Ensure the app not in debug mode'
-
-
-def test_visited_links(client):
+def test_visited_links_1(client):
     data = """
         {
             "links": [
@@ -25,7 +21,29 @@ def test_visited_links(client):
     assert response.json == {'status': 'ok'}
 
 
-def test_visited_domains(client):
+def test_visited_links_2(client):
+    data = """
+        {
+            "urls": [
+                "https://ya.ru",
+                "https://ya.ru?q=123",
+                "funbox.ru",
+                "https://stackoverflow.ru/questions/11828270/how-to-exit-the-vim-editor"
+            ]
+        }
+        """
+    response = client.post(url_for('visited_links'), content_type='application/json', data=data)
+    assert response.status_code == 400
+    assert response.json.get('status') == 'error: Bad Request'
+
+
+def test_visited_links_3(client):
+    response = client.get(url_for('visited_links'))
+    assert response.status_code == 405
+    assert re.match(r'error:', response.json.get('status'))
+
+
+def test_visited_domains_1(client):
     timestamp_to = int(datetime.timestamp(datetime.now())) + 5
     timestamp_from = timestamp_to - 10
     url = f'{url_for("visited_domains")}?&from={timestamp_from}&to={timestamp_to}'
@@ -33,3 +51,17 @@ def test_visited_domains(client):
     assert response.status_code == 200
     assert set(response.json.get('domains')) == {'stackoverflow.ru', 'ya.ru', 'funbox.ru'}
     assert response.json.get('status') == 'ok'
+
+
+def test_visited_domains_2(client):
+    timestamp_to = int(datetime.timestamp(datetime.now())) + 5
+    url = f'{url_for("visited_domains")}?&to={timestamp_to}'
+    response = client.get(url)
+    assert response.status_code == 400
+    assert response.json.get('status') == 'error: Bad Request'
+
+
+def test_visited_domains_3(client):
+    response = client.get(url_for("visited_domains"))
+    assert response.status_code == 400
+    assert response.json.get('status') == 'error: Bad Request'
